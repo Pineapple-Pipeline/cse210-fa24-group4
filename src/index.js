4c06d06f076850b7162c9b992c5f3e6d35292471
Tim Kraemer <tim022402@gmail.com>
2024-12-06 10:43:47 -0800

    changes to light/dark mode theme, better colors

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ const initializeDarkMode = () => {
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     document.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
-    themeToggle.textContent = newTheme === "dark" ? "𖤓" : "⏾";
+    themeToggle.textContent = newTheme === "dark" ? "L" : "D";
   });
 };
 
