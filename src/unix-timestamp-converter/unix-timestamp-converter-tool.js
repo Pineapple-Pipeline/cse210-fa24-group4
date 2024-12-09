63f1fc24f0110c68360ee86649d6dedbd3fcf2b9
Tim Kraemer <73266469+TimKraemer1@users.noreply.github.com>
2024-12-08 17:50:39 -0800

    Recent edits to unix time stamp converter, css loads before html, new tests, iso format support (#92)
    * feat: added iso format to unix timestamp converter
    
    * feat: added unit testing for timestamp converter
    
    * fix: prettified js files for timestamp converter
    
    * fix: html renders before css/js
    
    * ran prettier
    
    * ran prettier, updated jwt file?
    
    ---------
    
    Co-authored-by: Adrian Layer <adrian.layer@gmail.com>
    Co-authored-by: Laura Tian <lauratian@MacBook-Pro-3.local>
    Co-authored-by: Tim Kraemer <tim022402@gmail.com>

--- src/unix-timestamp-converter/unix-timestamp-converter-tool.js
+++ src/unix-timestamp-converter/unix-timestamp-converter-tool.js
@@ -12,7 +12,7 @@ class UnixTimestampConverterTool extends HTMLElement {
     // Set up the HTML structure when the element is added to the DOM
     this.innerHTML = `
             <link rel="stylesheet" href="unix-timestamp-converter/unix-timestamp-converter-tool.css"> 
-            <section class="tool-panel">
+            <section class="tool-panel" style="display:none;">
               <header class="tool-header">
                 <h3>UNIX Timestamp Converter</h3>
               </header>
