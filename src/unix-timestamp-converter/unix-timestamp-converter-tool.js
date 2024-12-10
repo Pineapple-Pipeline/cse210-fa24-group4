f1da1aa3de12f15cdd0f07b4ffb5b54df424ca9c
Adrian Layer <adrian.layer@gmail.com>
2024-12-10 14:12:38 -0800

    Fix: Vastly simplified unix conditional rendering

--- src/unix-timestamp-converter/unix-timestamp-converter-tool.js
+++ src/unix-timestamp-converter/unix-timestamp-converter-tool.js
@@ -6,12 +6,9 @@ class UnixTimestampConverterTool extends HTMLElement {
     this.convertToUtcBtn = null;
     this.convertToIsoBtn = null;
     this.convertToUnixBtn = null;
-    this.fromCopyBtn = null;
-    this.toCopyBtn = null;
-    this.toInputArea = null;
-    this.fromInputArea = null;
-    this.toOutputArea = null;
-    this.fromOutputArea = null;
+    this.copyBtn = null;
+    this.inputArea = null;
+    this.outputArea = null;
   }
 
   connectedCallback() {
@@ -26,21 +23,15 @@ class UnixTimestampConverterTool extends HTMLElement {
         </header>
         <hr />
 
-        <section class="from-tool-content">
-          <textarea class="from-input-area" placeholder="Enter UNIX timestamp (e.g., 999999)"></textarea>
+        <section class="tool-content">
+          <textarea class="input-area" placeholder="Paste a timestamp in UNIX, UTC, or ISO format here"></textarea>
           <div class="button-group">
             <button class="convert-to-utc-btn">Convert TO UTC</button>
             <button class="convert-to-iso-btn">Convert TO ISO</button>
           </div>
-          <textarea class="from-output-area" readonly></textarea>
-          <button class="from-copy-btn">Copy to Clipboard</button>
-        </section>
-
-        <section class="to-tool-content">
-          <textarea class="to-input-area" placeholder="Enter UTC/ISO time (e.g., 2021-10-01T00:00:00Z)"></textarea>
           <button class="convert-to-unix-btn">Convert TO UNIX</button>
-          <textarea class="to-output-area" readonly></textarea>
-          <button class="to-copy-btn">Copy to Clipboard</button>
+          <textarea class="output-area" readonly></textarea>
+          <button class="copy-btn">Copy to Clipboard</button>
         </section>
       </section>
     `;
@@ -51,12 +42,9 @@ class UnixTimestampConverterTool extends HTMLElement {
     this.convertToUtcBtn = this.querySelector(".convert-to-utc-btn");
     this.convertToIsoBtn = this.querySelector(".convert-to-iso-btn");
     this.convertToUnixBtn = this.querySelector(".convert-to-unix-btn");
-    this.fromCopyBtn = this.querySelector(".from-copy-btn");
-    this.toCopyBtn = this.querySelector(".to-copy-btn");
-    this.toInputArea = this.querySelector(".to-input-area");
-    this.fromInputArea = this.querySelector(".from-input-area");
-    this.toOutputArea = this.querySelector(".to-output-area");
-    this.fromOutputArea = this.querySelector(".from-output-area");
+    this.copyBtn = this.querySelector(".copy-btn");
+    this.inputArea = this.querySelector(".input-area");
+    this.outputArea = this.querySelector(".output-area");
 
     // Bind event listeners
     this.swapBtn.addEventListener("click", () => this.swapMode());
@@ -67,10 +55,7 @@ class UnixTimestampConverterTool extends HTMLElement {
       this.convertFromUnix("iso")
     );
     this.convertToUnixBtn.addEventListener("click", () => this.convertToUnix());
-    this.fromCopyBtn.addEventListener("click", () =>
-      this.copyToClipboard("from")
-    );
-    this.toCopyBtn.addEventListener("click", () => this.copyToClipboard("to"));
+    this.copyBtn.addEventListener("click", () => this.copyToClipboard());
   }
 
   swapMode = () => {
@@ -79,52 +64,47 @@ class UnixTimestampConverterTool extends HTMLElement {
 
   convertFromUnix = (format) => {
     try {
-      const input = this.fromInputArea.value;
+      const input = this.inputArea.value;
       const unixTimestamp = parseInt(input, 10);
 
       // More specific error handling
       if (isNaN(unixTimestamp)) {
-        throw new Error("Invalid Unix timestamp");
+        throw new Error("Error: Invalid Unix timestamp");
       }
 
       const date = new Date(unixTimestamp * 1000);
 
       // output based on format inputted
       if (format == "utc") {
-        this.fromOutputArea.value = date.toUTCString();
+        this.outputArea.value = date.toUTCString();
       } else if (format == "iso") {
-        this.fromOutputArea.value = date.toISOString();
+        this.outputArea.value = date.toISOString();
       }
     } catch (error) {
-      this.fromOutputArea.value = `Error: ${error.message}`;
+      this.outputArea.value = `${error.message}`;
     }
   };
 
   convertToUnix = () => {
     try {
-      const input = this.toInputArea.value;
+      const input = this.inputArea.value;
       const date = new Date(input);
 
       // catch invalid dates and formats
       if (isNaN(date.getTime())) {
-        this.toOutputArea.value = "Error: Invalid date format";
+        this.outputArea.value = "Error: Invalid date format";
       }
 
       // convert to unix timestamp
       const unixTimestamp = date.getTime() / 1000;
-      this.toOutputArea.value = unixTimestamp;
+      this.outputArea.value = unixTimestamp;
     } catch (error) {
-      this.toOutputArea.value = `Error: ${error.message}`;
+      this.outputArea.value = `${error.message}`;
     }
   };
 
-  copyToClipboard(state) {
-    let output = ""; // why does this only work if it's a let statement???
-    if (state == "from") {
-      output = this.fromOutputArea.value;
-    } else if (state == "to") {
-      output = this.toOutputArea.value;
-    }
+  copyToClipboard() {
+    const output = this.outputArea.value;
 
     if (output) {
       navigator.clipboard
@@ -151,13 +131,8 @@ class UnixTimestampConverterTool extends HTMLElement {
       );
     if (this.convertToUnixBtn)
       this.convertToUnixBtn.removeEventListener("click", this.convertToUnix);
-    if (this.fromCopyBtn)
-      this.fromCopyBtn.removeEventListener(
-        "click",
-        this.copyToClipboard("from")
-      );
-    if (this.toCopyBtn)
-      this.toCopyBtn.removeEventListener("click", this.copyToClipboard("to"));
+    if (this.copyBtn)
+      this.copyBtn.removeEventListener("click", this.copyToClipboard);
   }
 }
 
