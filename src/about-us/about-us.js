ac6ad3823c6f2f84a6f2aa70f9167d41a610aa88
Laura Tian <lauratian0147@gmail.com>
2024-12-11 00:50:18 -0800

    chore: ran prettier

--- src/about-us/about-us.js
+++ src/about-us/about-us.js
@@ -1,36 +1,36 @@
 class AboutUs extends HTMLElement {
-	/**
-	 * Lifecycle method that is called when the custom element is added to the DOM.
-	 * It sets up the inner HTML of the `AboutUs` component, which includes
-	 * rendering a team section with details of team members. The section consists
-	 * of a grid layout, where each team member is represented by their name and role.
-	 * Additionally, it includes styles for the component to ensure proper display.
-	 */
-	connectedCallback() {
-		const teamMembers = [
-			{ name: "Nikhil Gautam", role: "Project Manager" },
-			{ name: "Samyak Mehta", role: "Frontend Developer" },
-			{ name: "Jake Norbie", role: "Backend Developer" },
-			{ name: "Adrian Layer", role: "UX Designer" },
-			{ name: "Tim Kraemer", role: "Data Scientist" },
-			{ name: "Anusha Ravachandran", role: "QA Engineer" },
-			{ name: "Yunhao Jiang", role: "DevOps Engineer" },
-			{ name: "Laura Tian", role: "Technical Writer" },
-			{ name: "Hailey Li", role: "Scrum Master" },
-		];
+  /**
+   * Lifecycle method that is called when the custom element is added to the DOM.
+   * It sets up the inner HTML of the `AboutUs` component, which includes
+   * rendering a team section with details of team members. The section consists
+   * of a grid layout, where each team member is represented by their name and role.
+   * Additionally, it includes styles for the component to ensure proper display.
+   */
+  connectedCallback() {
+    const teamMembers = [
+      { name: "Nikhil Gautam", role: "Project Manager" },
+      { name: "Samyak Mehta", role: "Frontend Developer" },
+      { name: "Jake Norbie", role: "Backend Developer" },
+      { name: "Adrian Layer", role: "UX Designer" },
+      { name: "Tim Kraemer", role: "Data Scientist" },
+      { name: "Anusha Ravachandran", role: "QA Engineer" },
+      { name: "Yunhao Jiang", role: "DevOps Engineer" },
+      { name: "Laura Tian", role: "Technical Writer" },
+      { name: "Hailey Li", role: "Scrum Master" },
+    ];
 
-		const teamGrid = teamMembers
-			.map(
-				(member) => `
+    const teamGrid = teamMembers
+      .map(
+        (member) => `
           <div class="team-member">
             <h2>${member.name}</h2>
             <p>${member.role}</p>
           </div>
-        `
-			)
-			.join("");
+        `,
+      )
+      .join("");
 
-		this.innerHTML = `
+    this.innerHTML = `
       <link rel="stylesheet" href="about-us/about-us.css">
       <section class="about-us">
         <section class="tool-header">
@@ -44,7 +44,7 @@ class AboutUs extends HTMLElement {
         </div>
       </section>
     `;
-	}
+  }
 }
 
 // Register the custom element
